410715607ea11a2a97a08d456c80f4a7ac86fa11
Raghav Deshpande <dehpander26@gmail.com>
2025-03-25 21:36:07 +0530

    Added user and user role entities

---
+++ SkyMirror/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿namespace SkyMirror.Entities
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string PasswordHash { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        // Foreign Key
+        public int UserRoleId { get; set; }
+        public UserRole UserRole { get; set; }
+    }
+}
