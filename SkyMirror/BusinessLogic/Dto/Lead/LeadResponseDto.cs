393373ef88ed2106915e1d58211d05ba35652fe6
Raghav Deshpande <dehpander26@gmail.com>
2025-04-01 23:54:31 +0530

    Added Lead Dto

--- SkyMirror/BusinessLogic/Dto/Lead/LeadResponseDto.cs
+++ SkyMirror/BusinessLogic/Dto/Lead/LeadResponseDto.cs
@@ -2,5 +2,19 @@
 {
     public class LeadResponseDto
     {
+        public int LeadId { get; }
+        public int UserId { get; }
+        public string InquiryDetails { get; }
+        public string Status { get; }
+        public DateTime CreatedAt { get; }
+
+        public LeadResponseDto(int leadId, int userId, string inquiryDetails, string status, DateTime createdAt)
+        {
+            LeadId = leadId;
+            UserId = userId;
+            InquiryDetails = inquiryDetails;
+            Status = status;
+            CreatedAt = createdAt;
+        }
     }
 }
