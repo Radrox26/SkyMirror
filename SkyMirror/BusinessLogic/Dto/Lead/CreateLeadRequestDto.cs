7959beb87c4e74dab21813d19529501f2c239ea9
Raghav Deshpande <dehpander26@gmail.com>
2025-04-01 23:54:31 +0530

    Added Lead Dto

--- SkyMirror/BusinessLogic/Dto/Lead/CreateLeadRequestDto.cs
+++ SkyMirror/BusinessLogic/Dto/Lead/CreateLeadRequestDto.cs
@@ -2,5 +2,8 @@
 {
     public class CreateLeadRequestDto
     {
+        public int UserId { get; set; }
+        public string InquiryDetails { get; set; } = string.Empty;
+        public string Status { get; set; } = "New";
     }
 }
