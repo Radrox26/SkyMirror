82bc13f814537add074f365110fe1576cd53f27d
Raghav Deshpande <dehpander26@gmail.com>
2025-04-02 20:56:16 +0530

    Added remaining DTOs

--- SkyMirror/BusinessLogic/Dto/Payment/PaymentResponseDto.cs
+++ SkyMirror/BusinessLogic/Dto/Payment/PaymentResponseDto.cs
@@ -2,5 +2,19 @@
 {
     public class PaymentResponseDto
     {
+        public int PaymentId { get; }
+        public int OrderId { get; }
+        public decimal AmountPaid { get; }
+        public string PaymentStatus { get; } = string.Empty;
+        public DateTime PaymentDate { get; }
+
+        public PaymentResponseDto(int paymentId, int orderId, decimal amountPaid, string paymentStatus, DateTime paymentDate)
+        {
+            PaymentId = paymentId;
+            OrderId = orderId;
+            AmountPaid = amountPaid;
+            PaymentStatus = paymentStatus;
+            PaymentDate = paymentDate;
+        }
     }
 }
--- SkyMirror/BusinessLogic/Dto/QuotationItem/QuotationItemResponseDto.cs
+++ SkyMirror/BusinessLogic/Dto/QuotationItem/QuotationItemResponseDto.cs
@@ -2,5 +2,21 @@
 {
     public class QuotationItemResponseDto
     {
+        public int QuotationItemId { get; }
+        public int QuotationId { get; }
+        public int ProductId { get; }
+        public string ProductName { get; }
+        public int Quantity { get; }
+        public decimal Price { get; }
+
+        public QuotationItemResponseDto(int quotationItemId, int quotationId, int productId, string productName, int quantity, decimal price)
+        {
+            QuotationItemId = quotationItemId;
+            QuotationId = quotationId;
+            ProductId = productId;
+            ProductName = productName;
+            Quantity = quantity;
+            Price = price;
+        }
     }
 }
